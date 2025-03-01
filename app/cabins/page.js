39a416c66bfade71d9e2b1a811fc3f0f39503ca4
Arjun-1807 <arjun2005gowda@gmail.com>
2025-03-01 21:19:20 +0530

    individual cabin

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,4 +1,6 @@
-import CabinCard from "@/app/_components/CabinCard";
+import CabinList from "@/app/_components/CabinList";
+import Spinner from "@/app/_components/Spinner";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Cabins",
@@ -6,7 +8,6 @@ export const metadata = {
 
 export default function Page() {
   // CHANGE
-  const cabins = [];
 
   return (
     <div>
@@ -22,13 +23,9 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
